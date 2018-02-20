c6c723c70c5bd27c78909ce20e93001f770fdbe1
linlijian <praw__Rul2__lvluk@outlook.co.th>
2018-02-20 14:27:35 +0700

    Thresholding_Click

--- Assingment_one/Form1.Designer.cs
+++ Assingment_one/Form1.Designer.cs
@@ -28,12 +28,73 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.open = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.Thresholding = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // open
+            // 
+            this.open.Location = new System.Drawing.Point(703, 44);
+            this.open.Name = "open";
+            this.open.Size = new System.Drawing.Size(75, 23);
+            this.open.TabIndex = 0;
+            this.open.Text = "open";
+            this.open.UseVisualStyleBackColor = true;
+            this.open.Click += new System.EventHandler(this.open_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(26, 44);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(256, 256);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(304, 44);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(256, 256);
+            this.pictureBox2.TabIndex = 3;
+            this.pictureBox2.TabStop = false;
+            // 
+            // Thresholding
+            // 
+            this.Thresholding.Location = new System.Drawing.Point(699, 159);
+            this.Thresholding.Name = "Thresholding";
+            this.Thresholding.Size = new System.Drawing.Size(95, 23);
+            this.Thresholding.TabIndex = 4;
+            this.Thresholding.Text = "Thresholding";
+            this.Thresholding.UseVisualStyleBackColor = true;
+            this.Thresholding.Click += new System.EventHandler(this.Thresholding_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(842, 388);
+            this.Controls.Add(this.Thresholding);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.open);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button open;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button Thresholding;
     }
 }
 
--- Assingment_one/Form1.cs
+++ Assingment_one/Form1.cs
@@ -12,9 +12,81 @@ namespace Assingment_one
 {
     public partial class Form1 : Form
     {
+        private Bitmap f_image = null;  //input image
+        public Bitmap image;       //output image
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void open_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog OpenFileImage = new OpenFileDialog();
+            OpenFileImage.Filter = "bitmap (*.bmp)|*.bmp";
+            OpenFileImage.FilterIndex = 1;
+            if (OpenFileImage.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    if (f_image != null)
+                        f_image.Dispose();
+                    f_image = (Bitmap)Bitmap.FromFile(OpenFileImage.FileName, false);
+
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("Can not open file”, “File Error");
+                }
+            }
+            image = new Bitmap(f_image.Width, f_image.Height);
+
+
+            pictureBox1.Image = f_image;
+        }
+
+        private void Thresholding_Click(object sender, EventArgs e)
+        {
+            //threshold
+            int avg = 0;
+            int sum = 0;
+            int lastC = 0;
+
+            // Loop through the images pixels
+            for (int i = 0; i < f_image.Width; i++)
+            {
+                for (int j = 0; j < f_image.Height; j++)
+                {
+
+                    Color PixelColor = f_image.GetPixel(i, j);
+                    int C_gray = (int)(PixelColor.R + PixelColor.G + PixelColor.B) / 3;
+                    avg += C_gray;
+                    sum += 1;
+
+                    //check color
+                    lastC = C_gray;
+
+                }
+            }
+            int tmp = avg / sum;
+            for (int i = 0; i < f_image.Width; i++)
+            {
+                for (int j = 0; j < f_image.Height; j++)
+                {
+
+                    Color PixelColor = f_image.GetPixel(i, j);
+                    int C_gray = (int)(PixelColor.R + PixelColor.G + PixelColor.B) / 3;
+                    if (C_gray >= tmp)
+                    {
+                        image.SetPixel(i, j, Color.FromArgb(255, 255, 255));
+                    }
+                    else
+                    {
+                        image.SetPixel(i, j, Color.FromArgb(0, 0, 0));
+                    }
+
+                }
+            }
+            pictureBox2.Image = image;
         }
     }
 }
